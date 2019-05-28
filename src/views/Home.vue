2bacff0b053fa3576d8ee52968658aaa952a4b11
johnrobert_nelsoft <johnrobert.ferrer.nelsoft@gmail.com>
2019-05-28 15:12:06 +0800

    Initial Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>ZipInfo</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      My App
+    </ion-content>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  components: {
+  }
+}
+</script>
